7d76a4dcfedaed9cc19b8c01ea41f387f775ecd3
Puneet Thakur <punsttakur9132@gmail.com>
2021-08-20 17:33:07 +0530

    all commit

---
+++ forecast.js
@@ -0,0 +1,21 @@
+const key = "qZKrLGe1fHOVcvdDrmeFjJH7xq31lc0i";
+
+const getCity = async (city) => {
+  const url = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await axios.get(url + query);
+  const data = response.data;
+
+  return data[0];
+};
+
+const getWeather = async (id) => {
+  const url = "https://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await axios.get(url + query);
+  const data = response.data;
+
+  return data[0];
+};
